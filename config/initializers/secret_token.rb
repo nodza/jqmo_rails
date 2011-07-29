1764e66499786af8da51be46d7a61218a46027ee
Noel Hwande <noel.hwande@gmail.com>
2011-07-28 19:02:05 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmoblog::Application.config.secret_token = '0bee2387a3e4c6f25de0c7e08a1eccbf1d73edbf791742cb5bd588b5ef641b7d9a3263c15d55428c27e3feee5b4c0584923c3e768730a44acc2a926267d1a77f'
